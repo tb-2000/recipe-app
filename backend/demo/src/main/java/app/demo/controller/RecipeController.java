d3e5e58087c629ddd060e32c58a8bb36b3c0ec61
tb-2000 <tomblum@gmx.de>
2026-03-05 11:15:45 +0100

    implemented RESTful controller

--- backend/demo/src/main/java/app/demo/controller/RecipeController.java
+++ backend/demo/src/main/java/app/demo/controller/RecipeController.java
@@ -1,14 +1,59 @@
 package app.demo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class HelloController {
+import app.demo.service.RecipeService;
+import jakarta.validation.Valid;
+import app.demo.model.*;
 
-    @GetMapping("/")
-    public String sayHello() {
-        return "Hallo aus dem Rezepte-Backend!";
+@RestController
+@RequestMapping("/api/rezepte")
+public class RecipeController {
+	
+	@Autowired
+    private final RecipeService service;
+    
+    public RecipeController(RecipeService service) {
+    	this.service = service;
+    }
+    
+    @GetMapping
+    public List<OverviewDto> getAll(){
+    	return service.findAll();
+    }
+    
+    @GetMapping("/{id}")
+    public RecipeDetailDto getRecipe(@PathVariable Long id) {
+    	return service.findById(id);
+    }
+    
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public RecipeDetailDto create(@Valid @RequestBody RecipeCreateDto dto) {
+    	return service.create(dto);
+    }
+    
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public RecipeDetailDto update(@PathVariable Long id, @Valid @RequestBody RecipeCreateDto dto) {
+    	return service.update(id, dto);
+    }
+    
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id) {
+    	service.delete(id);
     }
 }
