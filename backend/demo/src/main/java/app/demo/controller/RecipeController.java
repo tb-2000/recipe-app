c15702976a53cf62543e75995e85192acb520c29
tb-2000 <tomblum@gmx.de>
2026-03-06 20:25:41 +0100

    added crossOrigin

--- backend/demo/src/main/java/app/demo/controller/RecipeController.java
+++ backend/demo/src/main/java/app/demo/controller/RecipeController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,8 @@ import app.demo.model.*;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(
+		origins = "http://localhost:5174")
 public class RecipeController {
 	
 	@Autowired
@@ -29,17 +32,17 @@ public class RecipeController {
     	this.service = service;
     }
     
-    @GetMapping
-    public String helloApi() {
-    	return "Welcome to api!";
-    }
+//    @GetMapping
+//    public String helloApi() {
+//    	return "Welcome to the website!";
+//    }
     
     @GetMapping("/rezepte")
     public List<OverviewDto> getAll(){
     	return service.findAll();
     }
     
-    @GetMapping("rezepte/{id}")
+    @GetMapping("/rezepte/{id}")
     public RecipeDetailDto getRecipe(@PathVariable Long id) {
     	return service.findById(id);
     }
@@ -50,15 +53,20 @@ public class RecipeController {
     	return service.create(dto);
     }
     
-    @PutMapping("rezepte/{id}")
+    @PutMapping("/rezepte/{id}")
     @ResponseStatus(HttpStatus.OK)
     public RecipeDetailDto update(@PathVariable Long id, @Valid @RequestBody RecipeCreateDto dto) {
     	return service.update(id, dto);
     }
     
-    @DeleteMapping("rezepte/{id}")
+    @DeleteMapping("/rezepte/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable Long id) {
     	service.delete(id);
+    }
+    
+    @GetMapping("/error")
+    public String error() {
+    	return "Error occurred!";
     }
 }
